a5b00930920de89fe586769ac701411e647b5da4
kexinlan <kexinlan@10.135.245.70>
2019-05-07 21:40:17 +0800

    xml解析结果存储

--- src/utils/XMLParseUtil.java
+++ src/utils/XMLParseUtil.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -14,14 +16,14 @@ public class XMLParseUtil {
 	}
 	
 	public List<HashMap<String, String>> parseXML(String inputPath) {
+		List<HashMap<String, String>> xmlContent = new ArrayList<>();
+		
 		SAXReader reader = new SAXReader();
 		
 		try {
 			Document document = reader.read(inputPath);
 			Element root = document.getRootElement();
 			Iterator it = root.elementIterator();
-			//Element element = root.element("name");
-			//String name = element.getText();
 			
 			while (it.hasNext()) {
 				System.out.print("*****************************\n");
@@ -32,29 +34,39 @@ public class XMLParseUtil {
 				for (Attribute attr : componentAttrs) {
 					System.out.print("属性名: " + attr.getName() + "   属性值: "
 							+ attr.getValue() + "\n");
+					
 				}
 				
 				Iterator itt = component.elementIterator();
 				
 				while (itt.hasNext()) {
+					HashMap<String, String> tempElement = new HashMap<>();
 					Element componentChild = (Element) itt.next();
 
 					System.out.print("节点名: " + componentChild.getName() + 
 							"   节点值: " + componentChild.getStringValue() + "\n");
+					tempElement.put("class", componentChild.getName());
 					
 					List<Attribute> componentChildAttrs = componentChild.attributes();
 					
 					for (Attribute attr : componentChildAttrs) {
 						System.out.print("属性名: " + attr.getName() + "   属性值: "
 								+ attr.getValue() + "\n");
+						
+						tempElement.put(attr.getName(), attr.getValue());
 					}
+					
+					xmlContent.add(tempElement);
 				}
-				List<HashMap<String, String>> xmlContent;
+				
+				
 			}
 			
 			System.out.print("*******************************\n");
 			
-			//return name;
+			//System.out.print(xmlContent);
+			
+			return xmlContent;
 		} catch (DocumentException e) {
 			e.printStackTrace();
 		}
