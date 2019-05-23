d053c023f89b2754e88941763059fc5ea7d5fdfc
kexinlan <kexinlan@10.136.206.114>
2019-05-23 20:16:11 +0800

    没有更新

--- src/ruleEntity/entity/Component.java
+++ src/ruleEntity/entity/Component.java
@@ -4,16 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Component {
-    Map<String, HashMap<String,String>> componentInfo=new HashMap<>();
-    public void setComponentInfo(){
-        //componentInfo.put();
-
-    }
-    public Map<String, HashMap<String,String>> getComponentInfo(){
-    	
-    	return null;
-
-    }
 
 
 }
